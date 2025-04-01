9e85e1c352852f03e5ffeaa3d29040363de7f772
daniyal-abbassi <slimshady3753@protonmail.com>
2025-04-01 18:25:13 +0330

    chore: setup view engine

--- app.js
+++ app.js
@@ -3,5 +3,11 @@ const app = express();
 
 
 
+app.set('view engine','ejs');
+
+
+app.get('/',(req,res)=>{
+    res.render('form')
+})
 
 app.listen(3000,()=>console.log('app is runnig on port: ',3000))
