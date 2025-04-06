b421032a9d0fd99e561d866f8a1e10c656a816b7
daniyal-abbassi <slimshady3753@protonmail.com>
2025-04-06 05:02:32 +0330

    feat: add serialize and de-serialoze user

--- passport-config.js
+++ passport-config.js
@@ -30,12 +30,39 @@ passport.use('local-signUp',new LocalStrategy({
     }
 ))
 
-
-
 // LOG-IN CONFIG
-
+passport.use('local-logIn',new LocalStrategy({
+    usernameField: 'username',
+    passwordField: 'password',
+    },async(username,password,done)=>{
+        try {
+            const userExisting = await User.findByUsername(username);
+            if(!userExisting) {
+                return done(null,false,{message: 'user not found!'})
+            }
+            //compare passwords
+            const passwordMatch = await bcrypt.compare(password,userExisting.password);
+            if(!passwordMatch) {
+                return done(null,false,{message: 'incorrect password!!!'})
+            }
+            return done(null,userExisting)
+        } catch (error) {
+            return done(error);
+        }
+    }
+))
 
 
 // SERIALIZE
-
+passport.serializeUser((user,done)=>{
+    done(null,user.user_id)
+})
 // DESERIALIZE
+passport.deserializeUser(async(user_id,done)=>{
+    try {
+        const user = User.findById(user_id);
+        return done(null,user);
+    } catch (error) {
+        return done(error);
+    }
+})
