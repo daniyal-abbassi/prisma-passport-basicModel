4df475176bd92609103c973579a1f21df2f621c9
daniyal-abbassi <slimshady3753@protonmail.com>
2025-04-06 04:45:31 +0330

    chore: add user model

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const pool = require('../db/pool');
+
+
+const User = {
+    findByUsername: async(username)=>{
+        try {
+            const results = await pool.query('SELECT * FROM users WHERE username=$1',[username]);
+            return results.rows[0];
+        } catch (error) {
+            console.error('ERROR IN USER.findByUsername: ',error);
+            throw error;
+        }
+    },
+    createUser: async(username,password)=>{
+        try {
+            const results = await pool.query('INSERT INTO users(username,password) VALUES($1,$2) RETURNING *',[username,password]);
+            return results.rows[0];
+        } catch (error) {
+            console.error('ERROR IN USER.createUser: ',error);
+            throw error;
+        }
+    }
+}
+
+
+module.exports=User;
