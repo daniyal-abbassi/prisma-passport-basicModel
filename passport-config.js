9ca25781201d5609c14ffb32e26b4abeb7e5007a
daniyal-abbassi <slimshady3753@protonmail.com>
2025-04-07 02:25:18 +0330

    feat: replace sql with prisma model

--- passport-config.js
+++ passport-config.js
@@ -2,8 +2,7 @@ const LocalStrategy = require('passport-local');
 const bcrypt = require('bcrypt');
 const passport = require('passport');
 //use model
-const User = require('./models/User');
-
+const dbClient = require('./models/dbClient');
 // SIGN-UP CONFIG
 passport.use('local-signUp',new LocalStrategy({
     usernameField: 'username',
@@ -12,7 +11,7 @@ passport.use('local-signUp',new LocalStrategy({
     }, async(req,username,password,done) => {
         try {
             //look for user
-            const existingUser = await User.findByUsername(username);
+            const existingUser = await dbClient.findUserWithUsername(username);
             //if user already exist
             if(existingUser) {
                 //return done
@@ -21,7 +20,7 @@ passport.use('local-signUp',new LocalStrategy({
             // hash the password
             const hashedPassword = await bcrypt.hash(password,10);
             //add to database
-            const user = await User.createUser(username,hashedPassword);
+            const user = await dbClient.createUser(username,hashedPassword);
             //pass user to done
             return done(null,user)
         } catch (error) {
@@ -36,7 +35,7 @@ passport.use('local-logIn',new LocalStrategy({
     passwordField: 'password',
     },async(username,password,done)=>{
         try {
-            const userExisting = await User.findByUsername(username);
+            const userExisting = await dbClient.findUserWithUsername(username);
             if(!userExisting) {
                 return done(null,false,{message: 'user not found!'})
             }
@@ -60,7 +59,7 @@ passport.serializeUser((user,done)=>{
 // DESERIALIZE
 passport.deserializeUser(async(user_id,done)=>{
     try {
-        const user = await User.findById(user_id);
+        const user = await dbClient.findUserById(user_id);
         return done(null,user);
     } catch (error) {
         return done(error);
