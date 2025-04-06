6acc924b83a89284f7208e2eaced9fef5f1fd9da
daniyal-abbassi <slimshady3753@protonmail.com>
2025-04-06 05:54:51 +0330

    feat: add passwrod confirmation

--- routes/signUpRouter.js
+++ routes/signUpRouter.js
@@ -1,17 +1,26 @@
-const {Router} = require('express');
+const { Router } = require('express');
 const passport = require('passport');
 const signUpRouter = Router();
 
-signUpRouter.get('/',(req,res)=>{
-    const errorMessages = {error: req.flash('error')}
-    res.render('signUp',{errorMsgs: errorMessages})
+signUpRouter.get('/', (req, res) => {
+    const errorMessages = { error: req.flash('error') }
+    res.render('signUp', { errorMsgs: errorMessages })
 })
 
-signUpRouter.post('/',passport.authenticate('local-signUp',{
+signUpRouter.post('/',  (req, res,next) => {
+    const { username, password, 'confirm-pass': confirmPassword } = req.body;
+    const errorMsgs = { error: [] };
+
+    if (password !== confirmPassword) {
+        errorMsgs.error.push('Passwords do not match.');
+        return res.render('signUp', { errorMsgs: errorMsgs });
+    }
+    next();
+}, passport.authenticate('local-signUp', {
     successRedirect: '/folders',
     failureRedirect: '/sign-up',
     failureFlash: true,
 }))
 
 
-module.exports=signUpRouter;
+module.exports = signUpRouter;
