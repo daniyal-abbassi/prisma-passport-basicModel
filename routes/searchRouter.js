5226b2e5bc75e00cd7a3ff3401cc4cb4e1cbe507
daniyal-abbassi <slimshady3753@protonmail.com>
2025-04-09 23:00:31 +0330

    feat: add folder-files to router

--- routes/searchRouter.js
+++ routes/searchRouter.js
@@ -1,8 +1,18 @@
 const {Router} = require('express');
 const searchRouter = Router();
+const dbClient = require('../models/dbClient');
 
-
-
+searchRouter.get('/',async(req,res)=>{
+    try {
+        const searchValue = req.query.value;
+        const folders = await dbClient.searchAllFolders(searchValue);
+        const files = await dbClient.searchAllFiles(searchValue);
+        res.json({folders,files})
+    } catch (error) {
+        console.error('Error SEARCHING ITEMS:', error);
+        res.status(500).send('Error searching items');
+    }
+})
 
 
 
