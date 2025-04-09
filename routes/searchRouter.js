7f6c711f3796680b7eea4a6d43e5c99f0bcca3a6
daniyal-abbassi <slimshady3753@protonmail.com>
2025-04-09 23:51:48 +0330

    feat: change tiny for search logic

--- routes/searchRouter.js
+++ routes/searchRouter.js
@@ -4,10 +4,11 @@ const dbClient = require('../models/dbClient');
 
 searchRouter.get('/',async(req,res)=>{
     try {
+        console.log('in the router')
         const searchValue = req.query.value;
         const folders = await dbClient.searchAllFolders(searchValue);
         const files = await dbClient.searchAllFiles(searchValue);
-        res.json({folders,files})
+        res.render('search',{title: 'Search',layout: './layouts/main',folders,files,searchValue})
     } catch (error) {
         console.error('Error SEARCHING ITEMS:', error);
         res.status(500).send('Error searching items');
